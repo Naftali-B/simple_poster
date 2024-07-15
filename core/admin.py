019f7a5d7b350cb178233d9be7feee855ce67fb3
Naftali-B <nbudamba@gmail.com>
2024-07-15 13:28:41 +0300

    url routes

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# from .models import User
+from posters.models import Poster
 
-# Register your models here.
+
+# admin.site.register(User)
+
+admin.site.register(Poster)
+
